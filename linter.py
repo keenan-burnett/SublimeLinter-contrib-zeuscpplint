80cc57bac2b41794ec3baa776cbf4db013f8538f
keenan-burnett <keenburn2004@gmail.com>
2019-02-22 18:33:44 -0500

    created own version of cpplint with some default filters.

--- linter.py
+++ linter.py
@@ -1,10 +1,32 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+from SublimeLinter.lint import Linter, util  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
 
 
-class __class__(Linter):
-    cmd = '__cmd__'
-    regex = r''
-    multiline = False
+class Zeuscpplint(Linter):
+    cmd = ('cpplint', '${args}', '${file}')
+    regex = r'^.+:(?P<line>\d+):\s+(?P<message>.+)'
+    tempfile_suffix = '-'
+    error_stream = util.STREAM_BOTH  # errors are on stderr
+    on_stderr = None  # handle stderr via split_match
     defaults = {
-        'selector': 'source.python'
+        'selector': 'source.c, source.c++',
+        '--filter=,': '-legal',
+        '--linelength=,': '120',
     }
+
+    def split_match(self, match):
+        """
+        Extract and return values from match.
+
+        We override this method so that the error:
+            No copyright message found.
+            You should have a line: "Copyright [year] <Copyright Owner>"  [legal/copyright] [5]
+        that appears on line 0 (converted to -1 because of line_col_base), can be displayed.
+
+        """
+
+        match, line, col, error, warning, message, near = super().split_match(match)
+
+        if line is not None and line == -1 and message:
+            line = 0
+
+        return match, line, col, error, warning, message, near
